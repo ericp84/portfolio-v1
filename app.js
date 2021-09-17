00715374563e7e9dbb3d78da41da1af64acd6b74
Eric Paoli <ericpaoli65@gmail.com>
2021-09-17 10:59:48 +0200

    create backend

--- app.js
+++ app.js
@@ -1,59 +1,41 @@
-const titreSpans = document.querySelectorAll('h1 span');
-const btns = document.querySelectorAll('.btn-first');
-const logo = document.querySelector('.logo');
-const medias = document.querySelectorAll('.bulle');
-const l1 = document.querySelector('.l1');
-const l2 = document.querySelector('.l2');
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-const box3 = document.querySelector('.box3');
-const box4 = document.querySelector('.box4');
-const box5 = document.querySelector('.box5');
-const box6 = document.querySelector('.box6');
-const box7 = document.querySelector('.box7');
-const box8 = document.querySelector('.box8');
-const box9 = document.querySelector('.box9');
-const box10 = document.querySelector('.box10');
-const text = document.querySelector('.text');
-const logo1 = document.querySelector('.logo1');
-const logo2 = document.querySelector('.logo2');
-const logo3 = document.querySelector('.logo3');
-const logo4 = document.querySelector('.logo4');
-const logo5 = document.querySelector('.logo5');
-const video = document.querySelector('.video');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-function showAlert() {
-    console.log("click detecté")
-}
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-window.addEventListener('load', () => {
-    const TL = gsap.timeline({paused: true});
+var app = express();
 
-    TL
-    .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "ease", y: -500}, 0.45 )
-    .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
-    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
-    .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
-    .from(logo, 1, {transform: "scale(1.5)", ease: "ease", y: -500}, '-=0')
-    .from(text, 1, {left: -50, opacity: 0, ease: "ease", x: -1000}, 0.1 )
-    .from(logo1, 1, {top: -50, opacity: 0, ease: "ease", y: -200}, 0.45 )
-    .from(logo2, 1, {top: -50, opacity: 0, ease: "ease", y: -200}, 0.45 )
-    .from(logo3, 1, {top: -50, opacity: 0, ease: "ease", y: -200}, 0.45 ) 
-    .from(logo4, 1, {top: -50, opacity: 0, ease: "ease", y: -200}, 0.45 ) 
-    .from(logo5, 1, {top: -50, opacity: 0, ease: "ease", y: -200}, 0.45 ) 
-    .from(video, 1.5, {top: -50, opacity: 0, ease: "power2.out", y: -200}, 0.45)
-    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box1, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box2, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box3, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box4, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box5, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box6, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box7, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box8, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box9, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
-    .staggerFrom(box10, 1, {bottom: -200, ease: "power2.out"}, 0.1, '-=1')
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-    TL.play();
-    
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

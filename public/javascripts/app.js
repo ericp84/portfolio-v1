33a923e3836ce71bc2d9a0eaf38e5579320baaef
Eric Paoli <ericpaoli65@gmail.com>
2021-09-19 20:51:13 +0200

    modale

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,6 +8,51 @@ const box1 = document.querySelectorAll('.box');
 const logo1 = document.querySelectorAll('.logo1');
 const video = document.querySelector('.video-demo');
 
+document.addEventListener('DOMContentLoaded', () => { 
+    const triggers = document.querySelectorAll('[aria-haspopup="dialog"]');
+    const doc = document.querySelector('.container-btns');
+  
+    const open = function (dialog) {
+      dialog.setAttribute('aria-hidden', false);
+      doc.setAttribute('aria-hidden', true);
+    };
+  
+    const close = function (dialog) {
+      dialog.setAttribute('aria-hidden', true);
+      doc.setAttribute('aria-hidden', false);
+    };
+  
+    triggers.forEach((trigger) => {
+      const dialog = document.getElementById(trigger.getAttribute('aria-controls'));
+      const dismissTriggers = dialog.querySelectorAll('[data-dismiss]');
+  
+      // open dialog
+      trigger.addEventListener('click', (event) => {
+        event.preventDefault();
+  
+        open(dialog);
+      });
+  
+      // close dialog
+      dismissTriggers.forEach((dismissTrigger) => {
+        const dismissDialog = document.getElementById(dismissTrigger.dataset.dismiss);
+  
+        dismissTrigger.addEventListener('click', (event) => {
+          event.preventDefault();
+  
+          close(dismissDialog);
+        });
+      });
+  
+      window.addEventListener('click', (event) => {
+        if (event.target === dialog) {
+          close(dialog);
+        }
+      });
+    });
+  });
+
+
 window.addEventListener('load', () => {
 
     const TL = gsap.timeline({paused: true});
